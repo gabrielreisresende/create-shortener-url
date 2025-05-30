80a9000bf358343bac465e401e260aa86307114d
Gabriel Resende <gabriellrr2005@gmail.com>
2025-05-29 21:19:52 -0300

    feat: Micronaut project created

---
+++ src/main/java/com/gabrielreisresende/Application.java
@@ -0,0 +1,10 @@
+package com.gabrielreisresende;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
